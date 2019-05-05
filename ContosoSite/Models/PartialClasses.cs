f02818d86ab673e992c419cf90550a8d842a6ffb
Wonil Park <wpark912@gmail.com>
2019-05-05 11:49:37 -0400

    MVC 5 Database First Approach

---
+++ ContosoSite/Models/PartialClasses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoSite.Models
+{
+    [MetadataType(typeof(StudentMetadata))]
+    public partial class Student
+    {
+    }
+
+    [MetadataType(typeof(EnrollmentMetadata))]
+    public partial class Enrollment
+    {
+    }
+}
